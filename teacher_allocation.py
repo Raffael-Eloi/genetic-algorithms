663a0b9c916af5f03833bd7c031d31078e60aceb
CarlosTuribio <carlospturibio@gmail.com>
2022-08-22 03:02:18 -0300

    crossover finished

--- teacher_allocation.py
+++ teacher_allocation.py
@@ -6,31 +6,30 @@ dataclasses = [[1, 27, 0], [2, 33, 0], [4, 25, 0], [4, 25, 1], [5, 42, 1], [1, 3
                [6, 30, 0], [1, 21, 0], [3, 25, 1], [3, 25, 0], [4, 44, 0], [5, 44, 1], [1, 25, 0], [3, 44, 0],
                [5, 44, 1]]
 
-dataClassroom = [50, 20, 30, 40, 40, 50, 50, 40]
+dataClassroom = [[0, 50], [0, 20], [0, 30], [0, 40], [0, 40], [1, 50], [1, 50], [1, 40]]
 
 
-def createRChromosome(classroom: list, weekdays: list, classes: list) -> str:
-    chromosome = classroom[random.randrange(0, len(classroom))][0]
-    chromosome += weekdays[random.randrange(0, len(weekdays))]
-    chromosome += classes[random.randrange(0, len(classes))][0]
-    return chromosome
+def twoPointCrossover(firstChromosome: list, secondChromosome: list):
+    firstChromosomeModified = firstChromosome[0][:4] + secondChromosome[0][4:8] + firstChromosome[0][8:]
+    secondChromosomeModified = secondChromosome[0][:4] + firstChromosome[0][4:8] + secondChromosome[0][8:]
+    return [firstChromosomeModified, secondChromosomeModified]
 
 
-def createChromosome(classroom: list, weekdays: list, classes: list) -> str:
-    chromosome = classroom[random.randrange(0, len(classroom))][0]
-    chromosome += weekdays[random.randrange(0, len(weekdays))]
-    chromosome += classes[0]
+def createChromosome(classroom: list, weekdays: list, classe: list) -> list:
+    randClassroom = random.randrange(0, len(classroom))
+    strChromosome = classroom[randClassroom][0]
+    strChromosome += weekdays[random.randrange(0, len(weekdays))]
+    strChromosome += classe[0]
+    chromosome = [strChromosome, classroom[randClassroom][1], classroom[randClassroom][2], classe[1][0], classe[1][1],
+                  classe[1][2]]
     return chromosome
 
 
 def inicialization(classroom: list, weekdays: list, classes: list) -> list:
     listOfChromosomes = []
-    for chromosome in range(67):
-        chromosome = createRChromosome(classroom, weekdays, classes)
-        listOfChromosomes.append(chromosome)
-    for chromosome in classes:
-        chromosome = createChromosome(classroom, weekdays, chromosome)
-        listOfChromosomes.append(chromosome)
+    for classe in classes:
+        classe = createChromosome(classroom, weekdays, classe)
+        listOfChromosomes.append(classe)
     return listOfChromosomes
 
 
@@ -51,7 +50,7 @@ def generateClassroom(quantityOfclassroom: int) -> list:
     for i in range(quantityOfclassroom):
         classroom.append(generateClassroomNumber(classroom))
     for i in range(len(classroom)):
-        classroom[i] = [classroom[i], dataClassroom[i]]
+        classroom[i] = [classroom[i], dataClassroom[i][0], dataClassroom[i][1]]
     return classroom
 
 
@@ -93,3 +92,77 @@ def generateClasses(quantityOfClasses: int) -> list:
     for i in range(len(classes)):
         classes[i] = [classes[i], dataclasses[i]]
     return classes
+
+
+def TeacherPerDay(chromosome: list, population: list) -> float:
+    for nChromosome in population:
+        if chromosome[3] == nChromosome[3] and chromosome[0][4:7] == nChromosome[0][4:7] \
+                and nChromosome[0] != chromosome[0]:
+            return 0.5
+    return 0
+
+
+def ClassPerDay(chromosome: list, population: list) -> float:
+    for nChromosome in population:
+        if chromosome[0][:3] == nChromosome[0][:3] and chromosome[0][4:7] == nChromosome[0][4:7] \
+                and nChromosome[0] != chromosome[0]:
+            return 0.5
+    return 0
+
+
+def ClassPerClassroom(chromosome: list) -> float:
+    if chromosome[2] <= chromosome[4]:
+        return 0.5
+    return 0
+
+
+def corretLocation(chromosome: list) -> float:
+    if chromosome[1] != chromosome[5]:
+        return 0.5
+    return 0
+
+
+def fitness(population: list):
+    nFitness = 0
+    for chromosome in population:
+        nFitness += TeacherPerDay(chromosome, population)
+        nFitness += ClassPerDay(chromosome, population)
+        nFitness += ClassPerClassroom(chromosome)
+        nFitness += corretLocation(chromosome)
+        chromosome.append(nFitness)
+        nFitness = 0
+
+
+def selection(population: list) -> list:
+    population = sorted(population, key=lambda x: x[6])
+    return population[:11]
+
+
+def crossover(population: list) -> list:
+    listPopulation = []
+    population[0].pop()
+    crossPopulation = population[1:]
+    newPopulation = [(population[0])]
+    print(newPopulation)
+    for i in range(len(crossPopulation)):
+        if (i % 2) == 0:
+            crossChromosomes = twoPointCrossover(crossPopulation[i], crossPopulation[i+1])
+            listPopulation.extend([crossChromosomes[0], crossPopulation[i][1], crossPopulation[i][2], crossPopulation[i][3], crossPopulation[i][4], crossPopulation[i][5]])
+            newPopulation.append(listPopulation)
+            listPopulation = []
+            listPopulation.extend([crossChromosomes[1], crossPopulation[i + 1][1], crossPopulation[i + 1][2], crossPopulation[i + 1][3], crossPopulation[i + 1][4], crossPopulation[i + 1][5]])
+            newPopulation.append(listPopulation)
+            listPopulation = []
+    return newPopulation
+
+
+def mutation(population: list) -> list:
+    print(0)
+
+
+def update(population: list) -> list:
+    print(0)
+
+
+def finishing(population: list) -> bool:
+    print(0)
